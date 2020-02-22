59fdb212d600dc5c00c1ac16656adc515957325b
Matthew Zhang <mlzhang@scu.edu>
2020-02-22 15:51:55 -0800

    Change hostEquals to Paypal

--- extension_tutorial/background.js
+++ extension_tutorial/background.js
@@ -10,7 +10,7 @@ chrome.runtime.onInstalled.addListener(function () {
         chrome.declarativeContent.onPageChanged.addRules([{
             conditions: [new chrome.declarativeContent.PageStateMatcher({
                 pageUrl: {
-                    hostEquals: 'developer.chrome.com'
+                    hostEquals: 'paypal.com'
                 },
             })],
             actions: [new chrome.declarativeContent.ShowPageAction()]
