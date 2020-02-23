3fa1b85b419e5bf56c25d4570222314ed6874b74
Maggie <maggiedong177@gmail.com>
2020-02-22 21:50:39 -0800

    popup but small and left
    popup but small and left

--- extension_tutorial/content.js
+++ extension_tutorial/content.js
@@ -1,6 +1,13 @@
-var s = document.createElement('script');
+var iFrame  = document.createElement ("iframe");
+iFrame.src  = chrome.extension.getURL ("popup1.html");
+
+document.body.insertBefore(iFrame, document.body.firstChild);
+
+
+/*var s = document.createElement('script');
 s.src = chrome.runtime.getURL('script.js');
 s.onload = function() {
     this.remove;
 };
 (document.head || document.documentElement).appendChild(s);
+*/
