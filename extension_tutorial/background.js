97185e643c871236dbd4c48081956722799e84b9
Maggie <maggiedong177@gmail.com>
2020-02-22 17:40:53 -0800

    opens on paypal
    asdfasf

--- extension_tutorial/background.js
+++ extension_tutorial/background.js
@@ -1,33 +1,10 @@
 let changeColor = document.getElementById('changeColor');
 
 chrome.runtime.onInstalled.addListener(function () {
-    chrome.storage.sync.set({
-        color: '#3aa757'
-    }, function () {
-        console.log('The color is green.');
-    });
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
-        chrome.declarativeContent.onPageChanged.addRules([{
-        conditions: [
-          new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: { hostPrefix: 'www.paypal.com/'
-            //, pathContains: 'checkout'
-        })]
-        actions: [ new chrome.declarativeContent.ShowPageAction() ]
-        }]);
-    });
+    chrome.declarativeContent.onPageChanged.addRules([{
+        conditions: [new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: {hostContains: 'paypal'},
+        })],
+        actions: [new chrome.declarativeContent.ShowPageAction()]
+      }]);
 });
-
-
-changeColor.onclick = function (element) {
-    let color = element.target.value;
-    chrome.tabs.query({
-        active: true,
-        currentWindow: true
-    }, function (tabs) {
-        chrome.tabs.executeScript(
-            tabs[0].id, {
-                code: 'document.body.style.backgroundColor = "' + color + '";'
-            });
-    });
-};
