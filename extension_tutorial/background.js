197bc5fa46cecf7a3285800d0c3645fcf0a6efa1
Maggie <maggiedong177@gmail.com>
2020-02-22 16:19:01 -0800

    arghhhh
    akslfj;l

--- extension_tutorial/background.js
+++ extension_tutorial/background.js
@@ -8,12 +8,12 @@ chrome.runtime.onInstalled.addListener(function () {
     });
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
         chrome.declarativeContent.onPageChanged.addRules([{
-            conditions: [new chrome.declarativeContent.PageStateMatcher({
-                pageUrl: {
-                    hostEquals: 'paypal.com'
-                },
-            })],
-            actions: [new chrome.declarativeContent.ShowPageAction()]
+        conditions: [
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { hostPrefix: 'www.paypal.com/'
+            //, pathContains: 'checkout'
+        })]
+        actions: [ new chrome.declarativeContent.ShowPageAction() ]
         }]);
     });
 });
