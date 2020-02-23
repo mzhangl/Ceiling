de8de92441e44a3b41f9779f4314b379d63b0e8f
Matthew Zhang <mlzhang@scu.edu>
2020-02-23 09:23:34 -0800

    Dynamic pricing through local storage

--- ext/popup1.js
+++ ext/popup1.js
@@ -1,19 +1,25 @@
-$(document).ready (jQueryMain);
+//$(document).ready (jQueryMain);
 
-// function getPrice(){
-//     return "15.60"
-// }
+chrome.storage.local.get("price", function (price) {
+    console.log("HEllo from local storage")
+    if (price['price']) {
+        console.log(price['price']);
+        jQueryMain(price['price'].slice(1, price['price'].indexOf("U")-1));
+    } else {
+        console.log("No price")
+    }
+})
 
-
-function roundPrice(){
-    var price = parseFloat(getPrice())
+function roundPrice(price_str) {
+    var price = parseFloat(price_str)
+    console.log(price)
     return (Math.ceil(price) - price).toFixed(2)
 }
 
 
-function jQueryMain () {
-    $("body").append ('<p>Added by jQuery</p>');
-    // $(".price").append(getPrice());
-    // $(".donate").append(roundPrice());
-    // $("#amnt").value = toString(roundPrice());
+function jQueryMain(price_str) {
+    console.log("Hello");
+    $(".price").append(price_str);
+    $(".donate").append(roundPrice(price_str));
+    $("#amnt").value = toString(roundPrice(price_str));
 }
